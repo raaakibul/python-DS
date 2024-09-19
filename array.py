a4970a19edba2180bb0f779e9b73b2e6a8b532ad
rakibul-islam <raakibul.uap@gmail.com>
2024-09-19 21:55:17 +0600

    numpy array

--- array.py
+++ array.py
@@ -4,3 +4,16 @@ b = np.array([5,6,7,8])
 
 print(a[0])
 print(b[1])
+
+c = np.array([
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+])
+
+print(c.shape)
+
+x = np.array([1,2,3,4])
+y = np.array([5,6,7,8])
+
+print(x+y)

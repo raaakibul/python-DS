2db7dc37528fb5dcedced7a8b13715c60762ba5e
rakibul-islam <raakibul.uap@gmail.com>
2024-09-21 22:13:42 +0600

    np functions

--- np-functions.py
+++ np-functions.py
@@ -1,10 +1,16 @@
 import numpy as np 
 
 a = np.array([
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [1,2,3,7],
+    [4,5,6,7],
+    [7,8,9,7]
 ])
 
 print(np.sin(a))
 print(np.cos(a))
+print(np.median(a))
+
+print(np.size(a))
+
+a = a.reshape((2,6))
+print(a)

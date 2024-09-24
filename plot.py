e977eb321124d2a96bbf7454caf8d110fc7134f2
rakibul-islam <raakibul.uap@gmail.com>
2024-09-24 23:35:20 +0600

    plot code added

--- plot.py
+++ plot.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 x = np.arange(-100,100,1)
-y = 0.5 * x**2 + 2 * x
+y1 = 0.5 * x ** 2 + 2 * x
+y2 = np.sin(x)* 2000
+y3 = np.log(x) * 1000
 
-plt.plot(x,y)
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.plot(x,y3)
 plt.show()

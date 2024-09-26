8a4abec97610cfb80b3642f145500a519d07289a
rakibul-islam <raakibul.uap@gmail.com>
2024-09-26 22:39:20 +0600

    subplot file and code added

--- subplot.py
+++ subplot.py
@@ -9,7 +9,12 @@ y2 = x ** 2 + 2 * x
 ax1 = plt.subplot(221)
 ax2 = plt.subplot(222)
 
+ax3 = plt.subplot(223)
+ax4 = plt.subplot(224)
+
 ax1.plot(x,y1)
 ax2.plot(x,y2)
-
+ax3.plot(x,y1,'r')
+ax4.plot(x,y2, 'g')
+plt.tight_layout()
 plt.show()

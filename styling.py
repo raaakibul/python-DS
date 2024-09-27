52c99342731bd0e92469f38d7e46398e7ac70340
rakibul-islam <raakibul.uap@gmail.com>
2024-09-27 23:19:32 +0600

    matplotlib styling

--- styling.py
+++ styling.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 
 # style.use('ggplot')
-style.use('fivethirtyeight')
+# style.use('fivethirtyeight')
+# style.use('dark_background')
+style.use('grayscale')
 x = np.arange(0,30,0.2)
 y =np.sin(x)
 

3e87f83778f02a36637cb7d65f0cb6da5ebdf36c
rakibul-islam <raakibul.uap@gmail.com>
2024-09-28 21:23:42 +0600

    styling plot

--- styling.py
+++ styling.py
@@ -5,14 +5,16 @@ from matplotlib import style
 # style.use('ggplot')
 # style.use('fivethirtyeight')
 # style.use('dark_background')
-style.use('grayscale')
+# style.use('grayscale')
 x = np.arange(0,30,0.2)
-y =np.sin(x)
-
+y1 =np.sin(x)
+y2 =np.cos(x)
 # plt.grid(True)
 plt.title('Sine function')
 # plt.suptitle('I am bigger')
 
 
-plt.plot(x, y)
+plt.plot(x, y1, label='sine')
+plt.plot(x, y2, label='cosine')
+plt.legend(loc='upper left')
 plt.show()

6351795a1bd1db81905706ca99ea53dc79999940
rakibul-islam <raakibul.uap@gmail.com>
2024-09-29 22:13:50 +0600

    histogram code added

--- histogram.py
+++ histogram.py
@@ -5,4 +5,9 @@ mu, sigma = 172, 8
 x = mu +  sigma * np.random.randn(10000)
 
 plt.hist(x,100, facecolor='blue', density=True)
+plt.xlabel('Heights')
+plt.ylabel('percentage of students')
+plt.title('Heights of students')
+plt.text(145,0.04, 'mu = 172, sig=8')
+plt.grid(True)
 plt.show()

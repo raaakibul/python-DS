5ac892449c578f0d2103b94a60ae83293844051f
rakibul-islam <raakibul.uap@gmail.com>
2024-09-30 21:19:07 +0600

    3d plot

--- 3-d-plot.py
+++ 3-d-plot.py
@@ -3,4 +3,21 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 ax = plt.axes(projection='3d')
+# z = np.linspace(0,30,100)
+# x = np.sin(z)
+# y = np.cos(z)
+
+def z_function(x,y):
+    return np.sin(np.sqrt(x**2 + y**2))
+
+x = np.linspace(-5,5,100)
+y = np.linspace(-5,5,100)
+
+# ax.plot3D(x,y,z)
+
+X, Y = np.meshgrid(x, y)
+Z = z_function(X, Y)
+
+ax.plot_surface(X, Y,Z)
+
 plt.show()

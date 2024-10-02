0394d919c7d758611d32dcedfa9b0b6270ec21ed
rakibul-islam <raakibul.uap@gmail.com>
2024-10-02 22:54:38 +0600

    pandas statistics code

---
+++ pandas-statistics.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+data = {
+    'name':['Ana', 'Bob', 'John', 'Mike'],
+    'Age':[24,26,28,29],
+    'height':[176,165,187,182],
+    'gender':['f','m','m','m']
+}
+
+df = pd.DataFrame(data)
+print(df.count())
+print(df['Age'].count())
+print(df['height'].mean())

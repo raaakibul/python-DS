45933592b0e6ec3de6559654aa360d70e003e23e
rakibul-islam <raakibul.uap@gmail.com>
2024-10-04 22:47:45 +0600

    pandas merging code from 2 dataframe

--- pandas-merging.py
+++ pandas-merging.py
@@ -1,11 +1,13 @@
 import pandas as pd 
 
 data = {
+    'SSN':[2,5,7,8],
     'name':['Ana', 'Bob', 'John', 'Mike'],
     'Age':[24,26,28,29]
 }
 
 data2 = {
+    'SSN':[1,2,3,4],
     'height':[176,165,187,182],
     'gender':['f','m','m','m']
 }
@@ -13,4 +15,6 @@ data2 = {
 df1 = pd.DataFrame(data)
 df2 = pd.DataFrame(data2)
 
-df = pd.merge(df1,df2)
+df = pd.merge(df1,df2, on='SSN', how='outer')
+df.set_index('SSN',inplace=True)
+print(df)
